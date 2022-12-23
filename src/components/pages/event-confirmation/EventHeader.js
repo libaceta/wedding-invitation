043a4d7354271c74cc02b5da6f0dc16ebcd53b54
leandro.ibaceta <leandro_ibaceta@tecso.coop>
2022-12-23 18:21:59 -0300

    Change colors for my birthday invitations

--- src/components/pages/event-confirmation/EventHeader.js
+++ src/components/pages/event-confirmation/EventHeader.js
@@ -6,7 +6,7 @@ const EventHeader = (props) => {
 
     return(
         <div className={styles.header}>
-            <h2>Rocio Y Matias</h2>
+            <h2>Cumple Pampa</h2>
             <h3>Confirmar asistencia</h3>
             <span className={styles.text}>Presiona "Si, contá conmigo" para ver el resto de los módulos del formulario</span>
         </div>
